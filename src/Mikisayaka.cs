09383ad9479640ab310e3e21e6a1e3e9acd50ecc
umiyuki <egoblock@infoseek.jp>
2014-03-17 21:08:13 +0900

    First Commit
    さやかさんを作成しまいた。
    Create MikiSayaka.

---
+++ src/Mikisayaka.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MikiSayaka
+{
+    class Mikisayaka
+    {
+        string firstname = "さやか";
+        string lastname = "美樹";
+        public string Name
+        {
+            get { return lastname+firstname; }
+        }
+
+        int age = 14;
+        public int Age
+        {
+            get { return age; }
+        }
+
+        bool is_human = true;
+        public bool IsHuman
+        {
+            get { return is_human; }
+        }
+
+        bool is_live = true;
+        public bool IsLive
+        {
+            get { return is_live; }
+        }
+
+        string witch_name = "Oktavia von Seckendorff";
+
+        float distance_to_soulgem = 0f;
+
+        string[] serihu = { "あたしってホントバカ", "舞い上がっちゃってますね、私！" };
+        //何か喋る
+        public void SaySomething()
+        {
+            Random rnd = new Random();
+            int rnd_num = rnd.Next(serihu.Length);
+
+            System.Console.WriteLine( serihu[rnd_num] );
+        }
+
+        //絶望
+        public void Despair()
+        {
+            is_human = false;
+            is_live = false;
+        }
+
+        //ソウルジェム投げる
+        public void ThrowedSoulgem()
+        {
+            distance_to_soulgem = 100f;
+
+            is_live = false;
+        }
+    }
+}
---
+++ src/Test.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MikiSayaka
+{
+    class Test
+    {
+        static void Main()
+        {
+            Mikisayaka sayakasan = new Mikisayaka();
+
+            System.Console.WriteLine("名前は" + sayakasan.Name);
+
+            Console.ReadKey();
+
+            System.Console.WriteLine("さやかさん、何か喋ってください");
+
+            sayakasan.SaySomething();
+
+            Console.ReadKey();
+
+            System.Console.WriteLine( "生存=" + sayakasan.IsLive + "  人間=" + sayakasan.IsHuman );
+
+            Console.ReadKey();
+
+            System.Console.WriteLine("さやかさんは絶望した");
+
+            sayakasan.Despair();
+
+            System.Console.WriteLine("生存=" + sayakasan.IsLive + "  人間=" + sayakasan.IsHuman );
+
+            Console.ReadKey();
+        }
+    }
+}
